db089f9406b671f5ae3de358c92bb0465b2fd1a4
tth010299 <tth010299@gmail.com>
2020-11-12 11:03:31 +0700

    [[thanhhai] fix order product2

--- PRC_Project.Data/ViewModels/OrderDetailModel.cs
+++ PRC_Project.Data/ViewModels/OrderDetailModel.cs
@@ -11,7 +11,5 @@ namespace PRC_Project.Data.ViewModels
         public string ProductId { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
-        public  OrderModel Order { get; set; }
-        public  ProductModel Product { get; set; }
     }
 }
--- PRC_Project.Data/ViewModels/OrderModel.cs
+++ PRC_Project.Data/ViewModels/OrderModel.cs
@@ -12,6 +12,6 @@ namespace PRC_Project.Data.ViewModels
         public string  Phone { get; set; }
         public string Note { get; set; }
         public double Total { get; set; }
-        public  List<OrderDetailModel> OrderDetail { get; set; }
+        public List<OrderDetailModel> OrderDetail { get; set; }
     }
 }
